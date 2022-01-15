72feae94438ecff18ed9a5678c72f91d088c3fec
Juan Pablo Cano <Canojuan0415@gmail.com>
2022-01-15 11:30:17 -0500

    Uso de Ajax, creación de botón eliminar y modificación de imagenes

--- js/component.js
+++ js/component.js
@@ -1,6 +1,6 @@
 function componenteCard(producto) {
     return `<div class="container px-4 px-lg-5 mt-5">
-                <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+                <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4">
                     <img src=${producto.imagen} height="200px" width="200px" class="card-img-top">
                 </div>
                 <div class="col-md-8">
@@ -9,7 +9,10 @@ function componenteCard(producto) {
                         <h5 class="card-title">${producto.nombre} <br>
                             <h6> $ ${producto.precio} USD </h6>
                         </h5>
-                        <button id=${producto.id} type="button" class="btn btn-dark"> Add to cart </button>
+                        <div class="d-grid gap-2 d-md-flex md-end">
+                            <button id=${producto.id} type="button" class="btn btn-dark"> Add</button>
+                            <button id=${producto.id} type="button" class="btn btn-dark btnDelete"> Eliminar </button>
+                        </div>
                     </div>
                 </div>
             </div>`
--- js/scripts.js
+++ js/scripts.js
@@ -15,28 +15,28 @@ class Producto {
 
 const listaProductos = [];
 
-const marca = ["Nike", "Jordan", "Puma", "Adidas", "Under armour"];
+const marca = ["Nike", "Jordan", "Puma", "Adidas", "UnderArmour"];
 
-listaProductos.push(new Producto(marca[0], "air-Force 1", 800, 'https://img01.ztat.net/article/spp-media-p1/d212dcea28a1366599c3b0874c2294e3/80367f1535d74e469487fe7d3c2029cb.jpg?imwidth=1800&filter=packshot alt="..."'));
-listaProductos.push(new Producto(marca[1], "retro 4", 1000, "https://essential.vteximg.com.br/arquivos/ids/330279-1000-1000/306-6638_1.jpg?v=637304226196930000 alt='...'"));
-listaProductos.push(new Producto(marca[2], "court 2k", 300, "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_2000,h_2000/global/376313/01/sv01/fnd/PNA/fmt/png/Court-Rider-2K-Basketball-Shoes alt='...'"));
-listaProductos.push(new Producto(marca[3], "proBounce", 500, "https://m.media-amazon.com/images/I/71rZecw6K+L._AC_UX395_.jpg alt='...'"));
-listaProductos.push(new Producto(marca[0], "kobe VI", 2300, "https://www.nicekicks.com/files/2020/12/lead-nike-zoom-kobe-6-grinch-cw2190-300-2021-release-date.jpg alt='...'"));
-listaProductos.push(new Producto(marca[1], "retro 1", 1780, "https://cdn.shopify.com/s/files/1/0603/3031/1875/products/554724-173_ta-L_1512x.jpg?v=1633758772 alt='...'"));
-listaProductos.push(new Producto(marca[3], "d Rose", 890, "http://desempacados.com/wp-content/uploads/2019/12/adidas-D-Rose-1-Roses-Grey-900x600.jpg alt='...'"));
-listaProductos.push(new Producto(marca[2], "fusion nitro Spectra", 130, "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_2000,h_2000/global/195684/01/sv01/fnd/PNA/fmt/png/Fusion-Nitro-Spectra-Basketball-Shoes alt='...'"));
-listaProductos.push(new Producto(marca[1], "retro 5", 1658, "https://static.nike.com/a/images/t_prod_ss/w_960,c_limit,f_auto/tsfwosn6q2a1ivflhkj6/air-jordan-5-retro-sail-racer-blue-release-date.jpg alt='...'"));
-listaProductos.push(new Producto(marca[0], "kobe VII", 2300, "https://m.media-amazon.com/images/I/71VnoJp3NxL._AC_UL1500_.jpg alt='...'"));
-listaProductos.push(new Producto(marca[4], "curry One", 1250, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3yEUft1jLuNo3WizaSKqoy5QQZKdiEkPOeA&usqp=CAU alt='...'"));
-listaProductos.push(new Producto(marca[0], "hyperdunk 2011", 2000, "https://images.solecollector.com/complex/image/upload/kfny2elbwwvrgxnvbjc7.jpg alt='...'"));
-listaProductos.push(new Producto(marca[0], "hyperdunk 2016 low", 1240, "https://m.media-amazon.com/images/I/61chZTph1VL._AC_UY395_.jpg alt='...'"));
-listaProductos.push(new Producto(marca[3], "pro model 2g", 450, "https://www.innvictus.com/medias/tenis-adidas-pro-model-2g-in-EF9821-1.png?context=bWFzdGVyfGltYWdlc3wyMTU0MTB8aW1hZ2UvcG5nfGltYWdlcy9oOWYvaDkyLzk2OTEyNjQ2NDcxOTgucG5nfGYwYWQzZjE1MmM2MmExZjE0NmQ4ZjkzMmUyMmUzMzMyZGU5MmE5ZmUzZTY2OGFjNmMzZWViYzE1NTE3NTM0MTc alt='...'"));
-listaProductos.push(new Producto(marca[0], "leBron soldier 9", 1879, "https://http2.mlstatic.com/nba-nike-tenis-lebron-soldier-9-tb-lebron-james-original-azl-D_NQ_NP_760578-MLM27297561563_052018-F.jpg alt='...'"));
-listaProductos.push(new Producto(marca[0], "kobe IX", 3599, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUPcJGnF7eOTNCcnEJ5rbYG1PrbYpRx_RxIQ&usqp=CAU alt='...'"));
-listaProductos.push(new Producto(marca[1], "retro XXI", 2500, "https://i8.amplience.net/i/jpl/jordan-xxi-7199ce15fc160487b15ace9674ea5cd8 alt='...'"));
-listaProductos.push(new Producto(marca[0], "kyrie 6", 899, "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/nike-kyrie-6-chinese-new-year-cd5029-700-6-1577703426.jpg?resize=768:* alt='...'"));
-listaProductos.push(new Producto(marca[0], "kd IV", 1699, "https://m.media-amazon.com/images/I/617UcJ+aqvL._AC_UY575_.jpg alt='...'"));
-listaProductos.push(new Producto(marca[0], "kobe VIII", 3799, "https://jgbasket.net/wp-content/uploads/2013/10/nike-kobe-8-viper-baloncesto.jpg alt='...'"));
+listaProductos.push(new Producto(marca[0], "air-Force 1", 800, 'https://u7.uidownload.com/vector/642/513/vector-right-sneaker-shoe-ai-eps-thumbnail.jpg alt="..."'));
+listaProductos.push(new Producto(marca[1], "retro 4", 1000, "https://u7.uidownload.com/vector/642/513/vector-right-sneaker-shoe-ai-eps-thumbnail.jpg alt='...'"));
+listaProductos.push(new Producto(marca[2], "court 2k", 300, "https://u7.uidownload.com/vector/642/513/vector-right-sneaker-shoe-ai-eps-thumbnail.jpg alt='...'"));
+listaProductos.push(new Producto(marca[3], "proBounce", 500, "https://u7.uidownload.com/vector/642/513/vector-right-sneaker-shoe-ai-eps-thumbnail.jpg alt='...'"));
+listaProductos.push(new Producto(marca[0], "kobe VI", 2300, "https://u7.uidownload.com/vector/642/513/vector-right-sneaker-shoe-ai-eps-thumbnail.jpg alt='...'"));
+listaProductos.push(new Producto(marca[1], "retro 1", 1780, "https://u7.uidownload.com/vector/642/513/vector-right-sneaker-shoe-ai-eps-thumbnail.jpg alt='...'"));
+listaProductos.push(new Producto(marca[3], "d Rose", 890, "https://u7.uidownload.com/vector/642/513/vector-right-sneaker-shoe-ai-eps-thumbnail.jpg alt='...'"));
+listaProductos.push(new Producto(marca[2], "fusion", 130, "https://u7.uidownload.com/vector/642/513/vector-right-sneaker-shoe-ai-eps-thumbnail.jpg alt='...'"));
+listaProductos.push(new Producto(marca[1], "retro 5", 1658, "https://u7.uidownload.com/vector/642/513/vector-right-sneaker-shoe-ai-eps-thumbnail.jpg alt='...'"));
+listaProductos.push(new Producto(marca[0], "kobe VII", 2300, "https://u7.uidownload.com/vector/642/513/vector-right-sneaker-shoe-ai-eps-thumbnail.jpg alt='...'"));
+listaProductos.push(new Producto(marca[4], "curry One", 1250, "https://u7.uidownload.com/vector/642/513/vector-right-sneaker-shoe-ai-eps-thumbnail.jpg alt='...'"));
+listaProductos.push(new Producto(marca[0], "hyperdunk", 2000, "https://u7.uidownload.com/vector/642/513/vector-right-sneaker-shoe-ai-eps-thumbnail.jpg alt='...'"));
+listaProductos.push(new Producto(marca[0], "hyperdunk", 1240, "https://u7.uidownload.com/vector/642/513/vector-right-sneaker-shoe-ai-eps-thumbnail.jpg alt='...'"));
+listaProductos.push(new Producto(marca[3], "proModel", 450, "https://u7.uidownload.com/vector/642/513/vector-right-sneaker-shoe-ai-eps-thumbnail.jpg alt='...'"));
+listaProductos.push(new Producto(marca[0], "leBronSoldier", 1879, "https://u7.uidownload.com/vector/642/513/vector-right-sneaker-shoe-ai-eps-thumbnail.jpg alt='...'"));
+listaProductos.push(new Producto(marca[0], "kobe IX", 3599, "https://u7.uidownload.com/vector/642/513/vector-right-sneaker-shoe-ai-eps-thumbnail.jpg alt='...'"));
+listaProductos.push(new Producto(marca[1], "retro XXI", 2500, "https://u7.uidownload.com/vector/642/513/vector-right-sneaker-shoe-ai-eps-thumbnail.jpg alt='...'"));
+listaProductos.push(new Producto(marca[0], "kyrie 6", 899, "https://u7.uidownload.com/vector/642/513/vector-right-sneaker-shoe-ai-eps-thumbnail.jpg alt='...'"));
+listaProductos.push(new Producto(marca[0], "kd IV", 1699, "https://u7.uidownload.com/vector/642/513/vector-right-sneaker-shoe-ai-eps-thumbnail.jpg alt='...'"));
+listaProductos.push(new Producto(marca[0], "kobe VIII", 3799, "https://u7.uidownload.com/vector/642/513/vector-right-sneaker-shoe-ai-eps-thumbnail.jpg alt='...'"));
 
 
 const generarProductos = document.getElementById('generarProductos');
@@ -79,14 +79,39 @@ filterProducts.onchange = function () {
     }
 }
 const filterByName = () => {
-    document.getElementById('productName').addEventListener('keyup', function (){
+    document.getElementById('productName').addEventListener('keyup', function () {
         const encontrados = listaProductos.filter(product => product.nombre.includes(this.value));
-        if (encontrados.length == 0){
+        if (encontrados.length == 0) {
             generarProductos.innerHTML = 'No se encontraron productos';
-        }else{
+        } else {
             $('#generarProductos').empty();
             mostrarProductos(encontrados);
         }
     })
 }
+
+$('#generarProductos').empty();
+mostrarProductos(listaProductos);
+
+
+$('#action-button').click(function () {
+    $.ajax({
+        url: 'https://api.joind.in/v2.1/talks/10889',
+        data: {
+            format: 'json'
+        },
+        error: function () {
+            $('#info').html('<p>An error has occurred</p>');
+        },
+        dataType: 'jsonp',
+        success: function (data) {
+            var $title = $('<h1>').text(data.talks[0].talk_title);
+            var $description = $('<p>').text(data.talks[0].talk_description);
+            $('#info')
+                .append($title)
+                .append($description);
+        },
+        type: 'GET'
+    });
+});
 filterByName();
